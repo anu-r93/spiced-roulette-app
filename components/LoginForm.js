fc0a0b694dc88b30d9bdc80f2419fb26ca94dced
Anushka Raman <anushkaraman93@gmail.com>
2024-06-10 19:11:31 +0200

    added padding in the form

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -14,7 +14,7 @@ const LoginForm = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-indigo-500">
-      <div className="w-full max-w-md">
+      <div className="w-full max-w-md p-8">
         <div className="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
           <div className="mb-6">
             <h1 className="text-3xl font-bold text-gray-800 mb-2">
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
-
-// const inter = Inter({ subsets: ["latin"] });
-
 import React, { useState } from "react";
-import LoginForm from "../components/LoginForm";
-import RegisterForm from "../components/RegisterForm";
-import HomePage from "./home/home";
 import LoginPage from "./login";
 const LoginAndRegistrationPage = () => {
-  // const [showLogin, setShowLogin] = useState(true);
-
-  // const toggleForm = () => {
-  //   setShowLogin(!showLogin);
-  // };
-
   return (
     <div>
       <LoginPage />
     </div>
-    /* {showLogin ? (
-        <LoginForm toggleForm={toggleForm} />
-      ) : (
-        <RegisterForm toggleForm={toggleForm} />
-      )} */
   );
 };
 
