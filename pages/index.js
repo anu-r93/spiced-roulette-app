9b1226b50fe743fea74a9059fd13f56582a191a0
Anushka Raman <anushkaraman93@gmail.com>
2024-06-11 17:38:16 +0200

    UI for NavBar created

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import LoginPage from "./login";
 const LoginAndRegistrationPage = () => {
   return (
---
+++ pages/roulette-I/index.js
@@ -0,0 +1,3 @@
+const RouletteSpinPage = () => {};
+
+export default RouletteSpinPage;
