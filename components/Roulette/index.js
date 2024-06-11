0f4aec576bb053528846e2bb6973d437fa36181c
Anushka Raman <anushkaraman93@gmail.com>
2024-06-11 20:52:47 +0200

    adjusted text and added margin bottom

--- components/Roulette/index.js
+++ components/Roulette/index.js
@@ -30,7 +30,7 @@ const RouletteFirstScreen = () => {
         onClick={onClick}
       >
         <h3 className="text-lg font-bold mb-2">{title}</h3>
-        <p className="text-gray-700">
+        <p className="text-slate-400">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
       </div>
@@ -39,7 +39,7 @@ const RouletteFirstScreen = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-purple-500 to-indigo-500 flex flex-col items-center py-8">
-      <div className="flex flex-wrap justify-center gap-4 mb-8 w-full px-4">
+      <div className="flex flex-wrap text-black justify-center gap-4 mb-8 w-full px-4">
         <Card
           title="Web Development"
           onClick={() => handleCardClick("Web Development")}
@@ -63,20 +63,20 @@ const RouletteFirstScreen = () => {
       </div>
       <div className="wheel relative">
         <Image
-          className={`transition-all duration-[5000ms] ease-[cubic-bezier(0.3,1,0.7,1)] will-change-transform rounded-full shadow-[0_0_100px_rgba(0,0,0,0.5)] max-w-[300px] ${
+          className={`transition-all duration-[5000ms] ease-[cubic-bezier(0.3,1,0.7,1)] will-change-transform rounded-full shadow-[0_0_100px_rgba(0,0,0,0.5)] max-w-[350px] ${
             isSpinning ? "animate-spin" : ""
           }`}
           src={RouletteWheelImage}
           alt="Roulette Wheel"
-          width={300}
-          height={300}
+          width={350}
+          height={350}
           style={{ transform: `rotate(${rotation}deg)` }}
         />
       </div>
       <div className="arrow absolute left-1/2 -translate-x-1/2 -translate-y-[50px] w-0 h-0 border-[40px] border-transparent border-t-[55px] border-t-tomato rounded-[0.35em] z-20" />
 
       <button
-        className="mt-8 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+        className="mt-8 mb-20 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
         type="submit"
         onClick={handleSpin}
         disabled={isSpinning}
