18f22e686557829ac4bc3ba92105f20ba79b1cbf
Anushka Raman <anushkaraman93@gmail.com>
2024-06-18 09:54:27 +0200

    added logout button with functionality

---
+++ components/Logout/index.js
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+
+const Logout = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Delete all cookies
+    document.cookie.split(";").forEach(function (c) {
+      document.cookie = c
+        .replace(/^ +/, "")
+        .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+    });
+
+    // Perform any additional logout actions here, such as clearing user data or state
+
+    // Redirect to the login page or any other desired page
+    router.push("/login");
+  };
+
+  return (
+    <button
+      className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 ml-4"
+      type="submit"
+      onClick={handleLogout}
+    >
+      Logout
+    </button>
+  );
+};
+
+export default Logout;
